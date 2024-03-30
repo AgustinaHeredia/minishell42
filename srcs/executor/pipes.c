2f3b0256a8ffd96d943497f7168303befe939155
Agustina Heredia Marengo <119814051+AgustinaHeredia@users.noreply.github.com>
2024-03-30 12:24:32 +0100

    ok 101% aprobado

--- srcs/executor/pipes.c
+++ srcs/executor/pipes.c
@@ -6,7 +6,7 @@
 /*   By: lbastien <lbastien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 00:52:41 by lbastien          #+#    #+#             */
-/*   Updated: 2024/03/25 12:49:35 by lbastien         ###   ########.fr       */
+/*   Updated: 2024/03/27 12:46:05 by lbastien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ int	assign_pipes(t_command *cmd, t_state *state)
 
 	if (pipe(pipe_fds) == -1)
 		return (0);
-	if (pipe_fds[0] > 255)
+	if (pipe_fds[0] > 256)
 	{
 		ft_error_exec(cmd->command, 1, "Too many Pipes", state);
-		close_open_fds(state);
+		close_next_fds(state->cmd_list);
 		return (0);
 	}
 	if (cmd->fd_out == STDOUT_FILENO)
@@ -57,34 +57,27 @@ void	make_dup(t_command *cmd, t_state *state)
 	{
 		if (dup2(cmd->fd_in, STDIN_FILENO) == -1)
 			ft_error_exec(cmd->command, code, "Failed to dup STDIN", state);
-		close(cmd->fd_in);
 	}
 	if (cmd->fd_out != STDOUT_FILENO)
 	{
 		if (dup2(cmd->fd_out, STDOUT_FILENO) == -1)
 			ft_error_exec(cmd->command, code, "Failed to dup STDOUT", state);
-		close(cmd->fd_out);
 	}
+	close_next_fds(cmd);
 }
 
-void	close_open_fds(t_state *state)
+void	close_next_fds(t_command *cmd)
 {
-	t_command	*cmd;
+	t_command	*current;
 
-	cmd = state->cmd_list;
-	while (cmd)
+	current = cmd;
+	while (current)
 	{
-		if (cmd->fd_out != STDOUT_FILENO && cmd->fd_out > 0)
-		{
-			close(cmd->fd_out);
-			cmd->fd_out = STDOUT_FILENO;
-		}
-		if (cmd->fd_in != STDIN_FILENO && cmd->fd_in > 0)
-		{
-			close(cmd->fd_in);
-			cmd->fd_in = STDIN_FILENO;
-		}
-		cmd = cmd->next;
+		if (current->fd_out != STDOUT_FILENO && current->fd_out >= 0)
+			close(current->fd_out);
+		if (current->fd_in != STDIN_FILENO && current->fd_in >= 0)
+			close(current->fd_in);
+		current = current->next;
 	}
 }
 
